06a066e3420841bb50f249d9956f88187f21785c
dmygrave <sasnabl@rambler.ru>
2021-11-07 14:51:15 +0300

    remove

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ filter.onchange = function () {
   for (let pic of pics) {
     if (pic.dataset.category !== filter.value) {         // оператор строгого НЕравенства1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       pic.classList.add('hidden')
+    } else {
+      pic.classList.remove('hidden')
     }
   }
 }
