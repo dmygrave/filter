ea6eb53267d070a3b19e764756dddd3cdc08ca1e
dmygrave <sasnabl@rambler.ru>
2021-11-07 15:01:05 +0300

    final, робит кажись

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let filter = document.querySelector('.filter');
 
 filter.onchange = function () {           
   for (let pic of pics) {
-    if (pic.dataset.category !== filter.value) {         // оператор строгого НЕравенства1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if (pic.dataset.category !== filter.value && filter.value !== 'all') {         // оператор строгого НЕравенства, к нему добавил логическое И, чтобы все картинки показывались
       pic.classList.add('hidden')
     } else {
       pic.classList.remove('hidden')
