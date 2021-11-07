6822ce0f00269f40212adbf8be1839afa7dc5dce
dmygrave <sasnabl@rambler.ru>
2021-11-07 14:17:42 +0300

    ы

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let filter = document.querySelector('.filter');
 
 
 
-filter.onchange = function () {           //обработчик событий
+filter.onchange = function () {           
   for (let pic of pics) {
-    if (pic.dataset.category === filter.value) {        //оператор строгого равенства + Валуев хеххее
+    if (pic.dataset.category === filter.value) {        
       pic.classList.add('highlight')
     }
   }
