162f9034f38f5800d15b4480c27878dfb9281cdd
dmygrave <sasnabl@rambler.ru>
2021-11-07 14:07:13 +0300

    добавление оператора ===

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ let pics = document.querySelectorAll('.fotochki');
 let filter = document.querySelector('.filter');
 
 for (let pic of pics) {
-  pic.classList.add('highlight')
+  if (pic.dataset.category === 'tyan') {        //оператор строгого равенства 
+    pic.classList.add('highlight')
+  }
+  
 }
