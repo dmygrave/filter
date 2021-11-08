777a8ece47721d8720862a04af87fda3d2bba799
dmygrave <sasnabl@rambler.ru>
2021-11-08 17:23:56 +0300

    кнопка, но не скрывается(

--- up-button.js
+++ up-button.js
@@ -1,5 +1,13 @@
 const upButton = document.querySelector('.up-button')
 
 window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+        upButton.classList.add('shown')
+    } else {
+        upButton.classList.remove('shown')
+    }
+}
 
+upButton.onclick = function () {
+    window.scrollTo(0, 0);
 }
